2f5594057f81d6ec4ffd27e92e390b9165e1ba40
Rahuldev009 <rahul.j.india@gmail.com>
2021-07-03 18:11:21 +0530

    Initial commit

---
+++ src/main/java/com/example/MediScreen/MediScreenApplication.java
@@ -0,0 +1,13 @@
+package com.example.MediScreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediScreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MediScreen/MediScreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MediScreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediScreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
